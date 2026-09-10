c6ff58c4b8a7d5ca8199c81d7b8e7e7be3661583
Pier Luc Ducharme <dutch80@gmail.com>
2026-09-10 14:45:25 -0400

    ajout exemples classe semaine 3 jeudi

--- Semaine 3/exemples en classe/jeudi/str_operations.py
+++ Semaine 3/exemples en classe/jeudi/str_operations.py
@@ -27,3 +27,61 @@ print("----" * 5 + "->" * 7)
 
 # Pour toutes les fonctions sur les str: https://docs.python.org/fr/3.14/library/stdtypes.html#string-methods
 
+# on peut faire du formattage et de la substitution avec format
+salutations = "Bonjour {}"
+print(salutations.format("Pier Luc"))
+print(salutations.format("Mustapha"))
+double_salutations = "Allo {} et {}"
+print(double_salutations.format("Pier Luc", "Mustapha"))
+
+# On peut aussi utiliser les format strings (fstrings) en préfixant la str avec f
+legume1 = "patate"
+legume2 = "carotte"
+
+print(f"Dans mon panier, j'ai une {legume1} et une {legume2}")
+# on peut utiliser format ou fstrings pour formatter des nombres
+nombre = 42.87654321
+print(f"{nombre:.5g}")
+print(f"{nombre:.6f}")
+
+ma_super_str = "ceci est ma super str. Célébrations!"
+print(ma_super_str.capitalize())
+
+print(ma_super_str.upper())
+print(ma_super_str.lower())
+
+# count(sous-chaîne) permet de compter le nombre de sous-chaînes dans la str
+str_repete = "Fais de ta vie un rêve et d'un rêve une réalité."
+print(str_repete.count("rêve"))
+# On peut commencer la recherche à partir d'un index et la terminé à une limite
+print(str_repete.count("rêve", 20))
+
+# On peut trouver l'index d'une sous-chaîne avec find() ou index()
+print(str_repete.find("rêve"))
+print(str_repete.find("patate"))
+print(str_repete.index("rêve"))
+# print(str_repete.index("patate"))
+
+print(str_repete.replace("rêve", "enfer"))
+
+# strip, lstrip, rstrip enlève les caractères au début et à la fin
+str_strip = "      Ceci est mon texte!     "
+print(str_strip.strip())
+print(str_strip.lstrip())
+print(str_strip.rstrip())
+autre_str = "->->Départ->->"
+print(autre_str.strip("->"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
